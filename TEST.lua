3362870f23617fb1566f396c9ba8c16dd8b1e737
Berkenens <78962912+Berkenens@users.noreply.github.com>
2026-05-30 16:30:27 +0300

    Update TEST.lua

--- TEST.lua
+++ TEST.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/Berkenens/UniversalAimbot/refs/heads/main/uimain.lua'))()
 
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
